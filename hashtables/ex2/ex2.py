ae2b19bfcbd3cf93045c819497c819687bfbe7c3
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-08-16 21:21:05 -0400

    Exercise 2 Done

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -4,11 +4,18 @@ class Ticket:
         self.source = source
         self.destination = destination
 
-
 def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
     # Your code here
-
+    tickets_dict = {}
+    for ticket in tickets:
+        tickets_dict[ticket.source] = ticket.destination
+    route = []
+    destination = tickets_dict["NONE"]
+    while destination != "NONE":
+        route.append(destination)
+        destination = tickets_dict[destination]
+    route.append(destination)
     return route
