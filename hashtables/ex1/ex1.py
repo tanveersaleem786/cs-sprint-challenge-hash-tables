fb971d5a5f57c629d527beabc25d2b064c0eb2c6
tanveersaleem786 <tanveer.saleem786@gmail.com>
2020-08-16 21:19:22 -0400

    Exercise 1 Done

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,14 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
-
+    weights_dict = {}
+    for (index, weight) in enumerate(weights):
+        weights_dict[weight] = index
+    for index, weight in enumerate(weights):
+        find_weight = limit - weight
+        if find_weight in weights_dict:
+            if index > weights_dict[find_weight]:
+                return (index, weights_dict[find_weight])
+            else:
+                return (weights_dict[find_weight], index)
     return None
